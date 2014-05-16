f5a668111c30732ce590c615fd04bfb3de22da01
atomixstudios <atomixstudios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-16 04:34:43 +0000

    version release
    git-svn-id: https://plugins.svn.wordpress.org/atom-linkedin/trunk@915250 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ atom-linkedin.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * @package atom-linkedin
+*/
+/*
+Plugin Name: Atom Linkedin
+Plugin URI: http://www.atomixstudios.com
+Description: Atom LinkedIn - Can show many LinkedIn widgets using One Module - LinkedIn Profile, LinkedIn Company Profile with Connections, Company Inside & Jobs You May Interested In.
+Version: 1.0.0
+Author: Thomas Andrew Erickson
+Author URI: http://www.atomixstudios.com
+*/
+class atom_linkedin extends WP_Widget{
+    public function __construct() {
+        $params = array(
+            'description' => 'Atom LinkedIn - Can show many LinkedIn widgets using One Module - LinkedIn Profile, LinkedIn Company Profile with Connections, Company Inside & Jobs You May Interested In.',
+            'name' => 'Atom LinkedIn'
+        );
+        parent::__construct('atom_linkedin','',$params);
+    }
+    public function form($instance) {
+        extract($instance);
+        ?>
+<p>
+    <label for="<?php echo $this->get_field_id('title');?>">Title: </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('title');?>"
+	name="<?php echo $this->get_field_name('title');?>"
+        value="<?php echo !empty($title) ? $title : "Atom Linkedin"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'linkedin_option' ); ?>">Choose the One you like to Show: </label> 
+    <select id="<?php echo $this->get_field_id( 'linkedin_option' ); ?>"
+        name="<?php echo $this->get_field_name( 'linkedin_option' ); ?>"
+        class="widefat" style="width:100%;">
+            <option value="CompanyProfile" <?php if ($linkedin_option == 'CompanyProfile') echo 'selected="CompanyProfile"'; ?> >Company Profile</option>
+            <option value="MemberProfile" <?php if ($linkedin_option == 'MemberProfile') echo 'selected="MemberProfile"'; ?> >Member Profile</option>
+            <option value="CompanyInsider" <?php if ($linkedin_option == 'CompanyInsider') echo 'selected="CompanyInsider"'; ?> >Company Insider</option>
+            <option value="JYMBII" <?php if ($linkedin_option == 'JYMBII') echo 'selected="JYMBII"'; ?> >Jobs You May Interested In</option>
+    </select>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('linkedin_id');?>">LinkedIn Company ID or Profile URL: </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('linkedin_id');?>"
+	name="<?php echo $this->get_field_name('linkedin_id');?>"
+        value="<?php echo !empty($linkedin_id) ? $linkedin_id : "1337"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'connections' ); ?>">Connections: </label> 
+    <select id="<?php echo $this->get_field_id( 'connections' ); ?>"
+        name="<?php echo $this->get_field_name( 'connections' ); ?>"
+        class="widefat" style="width:100%;">
+            <option value="true" <?php if ($connections == 'true') echo 'selected="true"'; ?> >True</option>
+            <option value="false" <?php if ($connections == 'false') echo 'selected="false"'; ?> >False</option>	
+    </select>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('suffix');?>">CSS Class Suffix : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('suffix');?>"
+	name="<?php echo $this->get_field_name('suffix');?>"
+    value="<?php echo !empty($suffix) ? $suffix : ""; ?>" />
+</p>
+<?php if($linkedin_option!= '' && $linkedin_id!= ''):?>
+<p><strong>Shortcode:</strong><br/>
+    <code>
+        [atom_linkedin linkedin_option="<?php echo $linkedin_option; ?>" linkedin_id="<?php echo $linkedin_id; ?>" connections="<?php echo $connections; ?>" suffix="<?php echo $suffix; ?>"]
+    </code>
+</p>
+<?php endif; ?>
+<?php
+    }
+    public function widget($args, $instance) {
+        extract($args);
+        extract($instance);
+        $title = apply_filters('widget_title', $title);
+        if(empty($title)) $title = "Atom Linkedin";
+        if(empty($linkedin_option )) $linkedin_option  = "CompanyProfile";
+        if(empty($linkedin_id )) $linkedin_id  = "1337";
+        if(empty($connections)) $connections = "true";
+        if(empty($suffix)) $suffix = "";
+        $data = $this->display_linkedin($linkedin_option,$linkedin_id,$connections);
+        echo $before_widget;
+        echo $before_title . $title . $after_title;
+        echo $data;
+        echo $after_widget;
+    }
+    public static function display_linkedin($linkedin_option,$linkedin_id,$connections){
+        $data = "";
+        $data .= "<script src='//platform.linkedin.com/in.js' type='text/javascript'></script>";
+        if($linkedin_option == "MemberProfile" || $linkedin_option == "CompanyProfile"){
+            $data .= "
+                <script type='IN/$linkedin_option' data-id='$linkedin_id' data-format='inline' data-related='$connections'></script>
+            ";
+        }else{
+            $data .= "
+                <script type='IN/$linkedin_option' data-id='$linkedin_id' data-format='inline'></script>
+            ";
+        }
+        return $data;
+    }
+}
+//register the widget
+add_action('widgets_init','register_atom_linkedin');
+function register_atom_linkedin(){
+    register_widget('atom_linkedin');
+}
+
+//shortcode
+add_shortcode('atom_linkedin', 'atom_linkedin_shortcode');
+
+function atom_linkedin_shortcode($atts){
+ 	$atts = shortcode_atts(array(
+ 		'linkedin_option' => 'CompanyProfile',
+                'linkedin_id' => '1337',
+                'connections' => 'true',
+                'suffix' => ''
+ 	), $atts);
+ 	extract($atts);
+        if(!empty($linkedin_option) && !empty($linkedin_id)){
+            $data = atom_linkedin::display_linkedin($linkedin_option, $linkedin_id, $connections);
+        } else{
+            $data = "linkedin_option and linkedin_id are required to display the shortcode";
+        }
+        return $data;
+ }
+ 
---
+++ readme.txt
@@ -0,0 +1,73 @@
+=== Atom Linkedin ===
+
+Plugin URI: http://atomixstudios.com
+Author Name : Thomas Andrew Erickson
+Author URL : http://atomixstudios.com
+Tags: linkedin widget, linkedin shortcode, linkedin plugin, linkedin widget for wordpress, linkedin shortcode for wordpress, linkedin wordpress, linkedin member profile, linkedin company profile, linkedin connections, linkedin company insider, linkedin Jobs You May Be Interested In
+Requires at least: 3.0.1
+Tested up to: 3.9.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Atom LinkedIn - Can show many LinkedIn widgets using One Module.
+
+== Description ==
+
+All Possible Just using One Linkedin Plugin for Wordpress.
+
+1) LinkedIn Member Profile
+
+2) LinkedIn Company Profile
+
+3) LinkedIn - Company Inside
+
+4) LinkedIn - Company (Jobs you may interested in)
+
+More info - http://atomixstudios.com/extensions/atom-linkedin.html
+
+== Installation ==
+
+After installing the plugin you can went to widget area or put the shortcode in page or post. This plugin require selecting Linkedin Option You like to show and linkedin ID or URL of Linkedin Profile. Remember Both you have to change to make sure the Linkedin plugin displays on your wordpress website.
+
+1. Choose the One you like to Show: Choose One among four. Atom LinkedIn is all in one Plugin for Wordpress. You can show LinkedIn Member Profile, Company Profile, Company Inside, Company Jobs you  may interested in - widget on your wordpress site. So that option mean you to choose the one radio field you like the plugin (widget or shortcode) to use. If like to show Company Profile than select "Company Profile" radio field. If like to show Member Profile than select "Member Profile"
+
+2. LinkedIn Profile URL or Company ID: If choose Member Profile - Than you have to put LinkedIn Profile URL on this field.
+
+Example: 
+http://au.linkedin.com/in/mattbarrie
+
+If choose Company Profile, Company Insider or Company Jobs you may Interested in than you have to put Company Profile ID. Wondering how can you grab the ID?
+
+It's very easy.
+
+go here -> https://developer.linkedin.com/apply-getting-started#company-lookup
+
+And put your company name - it will result the id.
+3. Connections: You can set it true or false. True will show Linkedin Connections connected with your LinkedIn World.
+
+Shortcode ->
+To Show Company Profile:
+[atom_linkedin linkedin_option="CompanyProfile" linkedin_id="1337" connections="true" suffix=""]
+
+To Show Company Insider
+[atom_linkedin linkedin_option="CompanyInsider" linkedin_id="1337" connections="true" suffix=""]
+
+To Show Jobs You May Be Interested In
+[atom_linkedin linkedin_option="JYMBII" linkedin_id="1337" connections="true" suffix=""]
+
+To Show Member Profile
+[atom_linkedin linkedin_option="MemberProfile" linkedin_id="http://au.linkedin.com/in/mattbarrie" connections="true" suffix=""]
+
+== Frequently Asked Questions ==
+ Contact us for any kind of support - thomas@atomixstudios.com
+
+== Screenshots ==
+
+1. Frontend Screenshot
+2. Widget Backend
+3. Shortcode
+
+== Changelog ==
+
+= 1.0.0 =
+Stable version release
