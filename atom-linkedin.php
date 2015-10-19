0ee7e4f73ab64e1a1f3feda979f3d8fe8bc7451e
atomixstudios <atomixstudios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-19 10:20:32 +0000

    atom-linkedin.php
    git-svn-id: https://plugins.svn.wordpress.org/atom-linkedin/trunk@1268509 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- atom-linkedin.php
+++ atom-linkedin.php
@@ -101,7 +101,7 @@ class atom_linkedin extends WP_Widget{
                 <script type='IN/$linkedin_option' data-id='$linkedin_id' data-format='inline'></script>
             ";
         }
-		$data .= "<div id='support_author' style='font-size: 8px; font-weight: normal;><a href='http://procurbappealconcrete.com' title='Visit the website' target='_blank'>Stamped Concrete Contractors</a></div>";
+		$data .= "<div id='support_author' style='font-size: 8px; font-weight: normal;><a href='http://www.quickrxrefill.com/' title='Visit the website' target='_blank'>Get a prescription online</a></div>";
         return $data;
     }
 }
